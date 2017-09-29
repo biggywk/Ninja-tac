089d49f3b4207d743b8651e4b445b22a9f309c0d
biggykw <big.baby-@hotmail.com>
2017-09-30 00:06:50 +0700

    add pic

--- Ninja tac.py
+++ Ninja tac.py
@@ -1,16 +1,22 @@
 
 import arcade
 SCREEN_WIDTH = 1200
-SCREEN_HEIGHT = 800
+SCREEN_HEIGHT = 720
+Scale = 1
 class NinjaWindow(arcade.Window):
     def __init__(self, width, height):
         super().__init__(width, height)
         self.background = arcade.load_texture("pics/sand back.jpg")
+        
+        
+        
     def on_draw(self):
-        arcade.start_render()
         arcade.draw_texture_rectangle(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2,
                                       SCREEN_WIDTH, SCREEN_HEIGHT, self.background)
-
+        arcade.start_render()
+        
+       
+        
 
 
 
